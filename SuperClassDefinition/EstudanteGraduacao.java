34e80e3110da120d0a89f0708e8f8eb1cbda1338
gabwestside <gabrielnfl13@gmail.com>
2021-12-05 21:10:54 -0300

    Att

--- SuperClassDefinition/EstudanteGraduacao.java
+++ SuperClassDefinition/EstudanteGraduacao.java
@@ -1,29 +1,22 @@
 public class EstudanteGraduacao {
-//TODOS OS ATRIBUTOS
-String nome, endereco, atividade;
+  // TODOS OS ATRIBUTOS
+  String nome, endereco, atividade, curso;
 
-int matricula, CPF, curso;// se estiver no primeiro semestre não está apto
+  int matricula, CPF;// se estiver no primeiro semestre não está apto
 
-float ide, renda_familiar, carga_horaria;
+  float ide, renda_familiar, carga_horaria;
 
-boolean status = true, bolsista = false;// status ativo ou inativo
-	
-	//Mostrar informações cadastradas do candidato
-	void Info() {
+  boolean status = true, bolsista = false;// status ativo ou inativo
+
+  // Mostrar informações cadastradas do candidato
+  void Info() {
     System.out.println("Nome do estudante: " + nome);
     System.out.println("Matricula: " + matricula);
-    System.out.println("O CPF: "+ CPF);
+    // System.out.println("O CPF: " + CPF);
     System.out.println("Endereço: " + endereco);
     System.out.println("Renda familiar: " + renda_familiar);
     System.out.println("Curso: " + curso);
-    System.out.println("Indice de Desenvolvimento do Estudante (IDE):"+ide);
-      if(status == true) {
-        atividade = "Ativo";
-      }
-      else {
-        atividade = "Inativo";
-      }
-
+    System.out.println("Indice de Desenvolvimento do Estudante (IDE):" + ide);
     System.out.println("Carga horaria: " + carga_horaria);
-	}
+  }
 }
