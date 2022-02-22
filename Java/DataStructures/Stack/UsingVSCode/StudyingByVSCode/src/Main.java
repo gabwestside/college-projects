8c65ca4921f253298ad832fd5fe37bd9464cc2ba
Gabriel Moura <gabriel.moura@fitbank.com.br>
2022-02-22 16:11:39 -0300

    Try to start interpole stack

--- Java/DataStructures/Stack/UsingVSCode/StudyingByVSCode/src/Main.java
+++ Java/DataStructures/Stack/UsingVSCode/StudyingByVSCode/src/Main.java
@@ -1,40 +1,92 @@
 public class Main {
     public Main() {
+        Pilha p = new Pilha();
+
+        p.push(80);
+        p.push(72);
+        p.push(75);
+        p.push(80);
+
         Pilha p1 = new Pilha();
 
-        p1.push(45);
-        p1.push(36);
-        p1.push(72);
-        p1.push(80);
+        p1.push(1);
+        p1.push(3);
+        p1.push(5);
 
-        this.removeElement(p1, 36);
-        this.print(p1);
+        Pilha p2 = new Pilha();
+
+        p2.push(2);
+        p2.push(4);
+        p2.push(6);
+
+        this.intertpoleStack(p1, p2);
+
+        System.out.println(this.isEquals(p));
+        // this.removeElement(p1, 36);
+        this.print(p);
     }
 
     public static void main(String[] args) {
         Main main = new Main();
     }
 
-    public void removeElement(Pilha p, int element) {
+    public Pilha intertpoleStack(Pilha p1, Pilha p2) {
+        Pilha union = new Pilha();
+
+        if (!p1.empty() && !p2.empty())
+            return union;
+
+        while (!p1.empty() && !p2.empty()) {
+            union.push(p1.pop());
+            union.push(p2.pop());
+        }
+
+        System.out.println(union);
+    }
+
+    // Verificar se a pilha é um palindromo
+    public boolean isEquals(Pilha p) {
         Pilha aux = new Pilha();
 
-        // Verificando se a pilha est� cheia
         if (p.empty() == true)
+            return false;
+
+        while (p.size() > aux.size()) {
+            aux.push(p.pop());
+        }
+
+        if (aux.size() != p.size())
+            aux.pop();
+
+        while (!p.empty()) {
+            if (p.pop() != aux.pop())
+                return false;
+        }
+
+        return true;
+    }
+
+    // Remover um elemento de determinada posição da pilha
+    public void removeElement(Pilha p, int pos) {
+        Pilha aux = new Pilha();
+
+        // Verificando se a pilha está cheia
+        if (p.empty())
             return;
 
-        // while (p.empty() == false) {
-        // aux.push(p.pop());
-        // }
+        // Verificar se a posição é valida
+        if (pos < 0 || pos >= p.size())
+            return;
 
-        // // Reempilhando a p a partir da aux
-        // while (aux.empty() == false) {
-        // if (element == aux.pop()) {
-        // p.push(aux.pop());
-        // }
-        // }
+        // Desemplihar os elementos até a posição pos
+        while (p.size() > pos) {
+            aux.push(p.pop());
+        }
 
-        p.pop();
+        aux.pop();
 
+        while (aux.empty() != true)
+            p.push(aux.pop());
     }
 
     public void replaceElement(Pilha p, int element, int pos) {
