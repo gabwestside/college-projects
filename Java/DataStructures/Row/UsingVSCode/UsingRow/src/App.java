8dea58820e3dd316c180000028b8d7345378d549
Gabriel Moura <gabrielnfl13@gmail.com>
2022-03-21 17:04:21 -0300

    Implements row

--- Java/DataStructures/Row/UsingVSCode/UsingRow/src/App.java
+++ Java/DataStructures/Row/UsingVSCode/UsingRow/src/App.java
@@ -1,10 +1,25 @@
 public class App {
     public App() {
+        Row row = new Row();
+        row.enqueue(14);
+        row.enqueue(22);
+        row.enqueue(40);
+        row.enqueue(51);
+        row.enqueue(55);
+        row.enqueue(60);
 
+    }
+
+    public void print(Row r) {
+        int length = r.size();
+        for (int i = 0; i < length; i++) {
+            int value = r.dequeue();
+            System.out.println(value + "-");
+            r.enqueue(value);
+        }
     }
 
     public static void main(String[] args) throws Exception {
         App app = new App();
     }
-
 }
--- Java/DataStructures/Row/UsingVSCode/UsingRow/src/Row.java
+++ Java/DataStructures/Row/UsingVSCode/UsingRow/src/Row.java
@@ -1,3 +1,52 @@
-public class Row {
+import javax.print.attribute.standard.RequestingUserName;
+import javax.swing.text.html.HTMLDocument.BlockElement;
 
+public class Row {
+    private int capacity = 100;
+
+    private int vetor[] = new int[capacity];
+
+    private int end = 0;
+
+    private int start = 0;
+
+    private int length = 0;
+
+    public int size() {
+        return length;
+    }
+
+    public boolean empty() {
+        return (length == 0);
+    }
+
+    public boolean full() {
+        return (length == capacity);
+    }
+
+    public int front() {
+        if (!empty())
+            return vetor[start];
+
+        return -1;
+    }
+
+    public void enqueue(int elem) {
+        if (size() < capacity) {
+            vetor[end] = elem;
+
+            end = (end + 1) % capacity;
+            length++;
+        }
+    }
+
+    public int dequeue() {
+        if (!empty()) {
+            int value = vetor[start];
+            start = (start + 1) % capacity;
+            length--;
+            return value;
+        }
+        return -1;
+    }
 }
