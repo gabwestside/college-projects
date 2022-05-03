91f03815264bab667f4c6c127040d270a96eff11
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-05-02 23:13:33 -0300

    adding new exercises

--- Java/DataStructures/LSE/LSE-VSCode-Version/LSE-HomeEdition/src/App.java
+++ Java/DataStructures/LSE/LSE-VSCode-Version/LSE-HomeEdition/src/App.java
@@ -1,9 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        App app = new App();
-    }
 
-    
 	public App() {
 		LSE lista = new LSE();
 		lista.inserirNoFinal(12);
@@ -11,5 +7,12 @@ public class App {
 		lista.inserirNoFinal(26);
 		lista.inserirNoFinal(32);
 		lista.printRecursivo(lista.head);
+		System.out.println();
+		lista.insertAtPos(0, 20);
+		lista.printIterativo();
+	}
+
+	public static void main(String[] args) throws Exception {
+		App app = new App();
 	}
 }
--- Java/DataStructures/LSE/LSE-VSCode-Version/LSE-HomeEdition/src/LSE.java
+++ Java/DataStructures/LSE/LSE-VSCode-Version/LSE-HomeEdition/src/LSE.java
@@ -1,37 +1,126 @@
 public class LSE {
-    public Node head = null;
-	
-	public void inserirNoFinal(int valor) {
-		//Criando um novo node
-		Node novo = new Node(valor);
-		
-		//Se a lista está vazia, head será esse novo nó
-		if(this.head == null) {
-			this.head = novo;
-		}
-		else {
+	public Node head = null;
+
+	public void insertAtPos(int pos, int value) {
+
+		Node node = new Node(value);
+
+		// Se a lista está vazia, head será esse node nó
+		if (this.head == null)
+			this.head = node;
+
+		if (pos == 0) {
+			node.next = head;
+			head = node;
+		} else {
 			Node aux = this.head;
-			//Percorrendo a lista até o último nó
-			while(aux.next != null) {
+
+			for (int i = 0; i < pos - 1; i++) {
 				aux = aux.next;
 			}
-			//Inserindo o novo nó no final da lista
+
+			node.next = aux.next;
+			aux.next = node;
+
+			// Percorrendo a lista até o último nó
+			while (node.next != null) {
+				node = node.next;
+			}
+		}
+	}
+
+	public int buscar(int pos) {
+		Node aux = head;
+
+		for (int i = 0; i < pos; i++) {
+			aux = aux.next;
+		}
+
+		return aux.info;
+	}
+
+	public void removerValor(int info) {
+		// Considere que n�o h� repeti��o de valor nos
+		// n�s
+
+		boolean flag = false;
+
+		// A info buscada � a do primeiro node (head)
+		if (head.info == info) {
+			head = head.next;
+			return;
+		}
+
+		Node aux = head;
+		// Posicionando o aux
+		while (aux.next.info != info && aux.next != null) {
+			aux = aux.next;
+			// Valor n�o encontrado
+			if (aux.next == null) {
+				flag = false;
+			} else {
+				flag = true;
+			}
+		}
+
+		// "Removendo" o node
+		if (flag)
+			aux.next = aux.next.next;
+	}
+
+	public void removerAtPos(int pos) {
+		Node aux = head;
+
+		if (head == null || pos < 0)
+			return;
+
+		// Se pos = 0, remover o n� inicial
+		if (pos == 0) {
+			head = head.next;
+			return;
+		}
+
+		// Posicionando o aux no n� anterior � posi��o
+		// desejada
+		for (int i = 0; i < pos - 1; i++) {
+			aux = aux.next;
+		}
+
+		// "Removendo" o node
+		aux.next = aux.next.next;
+	}
+
+	public void inserirNoFinal(int valor) {
+		// Criando um novo node
+		Node novo = new Node(valor);
+
+		// Se a lista está vazia, head será esse novo nó
+		if (this.head == null) {
+			this.head = novo;
+		} else {
+			Node aux = this.head;
+			// Percorrendo a lista até o último nó
+			while (aux.next != null) {
+				aux = aux.next;
+			}
+			// Inserindo o novo nó no final da lista
 			aux.next = novo;
 		}
 	}
-	
+
 	public void printIterativo() {
 		Node aux = this.head;
-		
-		//Percorrer a lista
-		while(aux != null) {
+
+		// Percorrer a lista
+		while (aux != null) {
 			System.out.print(aux.info + "-");
 			aux = aux.next;
 		}
 	}
-	
+
 	public void printRecursivo(Node auxHead) {
-		if(auxHead == null) return;
+		if (auxHead == null)
+			return;
 		Node aux = auxHead;
 		System.out.print(aux.info + "-");
 		printRecursivo(aux.next);
