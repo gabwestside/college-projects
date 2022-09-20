b2a77a1242ac77fe2feb6dc4ec3ad27bc4da5520
Gab <gabrielnfl13@gmail.com>
2022-09-19 21:18:21 -0300

    implementação da arvore binaria

--- Java/DataStructuresII/src/BinaryTree.java
+++ Java/DataStructuresII/src/BinaryTree.java
@@ -30,17 +30,87 @@ public class BinaryTree {
 			}
 		}
 	}
-	
+
 	Node buscar(Node root, int value) {
-		if(value == root.info) {
+		if (value == root.info) {
 			return root;
 		}
-		if(value < root.info) {
+		if (value < root.info) {
 			return buscar(root.left, value);
-		}
-		else {
+		} else {
 			return buscar(root.right, value);
 		}
 	}
-	
+
+	Node maximum(Node root) {
+		while (root.right != null) {
+			root = root.right;
+		}
+
+		return root;
+	}
+
+	Node minimum(Node root) {
+		while (root != null) {
+			root = root.right;
+		}
+
+		return root;
+	}
+
+	Node sucessor(Node root) {
+		Node rootTree = new Node();
+
+		if (root.right != null)
+			return minimum(root.right);
+
+		rootTree = rootTree.parent;
+
+		while (rootTree.info < root.info) {
+			root = rootTree;
+			rootTree = rootTree.parent;
+		}
+
+		rootTree = rootTree.parent;
+
+		return rootTree;
+	}
+
+	Node remove(int value, Node root) {
+		Node node = buscar(root, value);
+
+		if (node.right == null && node.left == null) {
+			if (node.info > node.parent.info) {
+				node.parent.right = null;
+			} else {
+				node.parent.left = null;
+			}
+		}
+		
+		if (node.right == null || node.left == null) {
+			if (node.info > node.parent.info) {
+				if (node.left == null) {
+					node.parent.right = node.right;
+					node.right.parent = node.parent;
+				} else {
+					node.parent.right = node.left;
+					node.left.parent = node.parent;
+				}
+			} else {
+				if (node.right == null) {
+					node.parent.left = node.left;
+					node.left.parent = node.parent;
+				} else {
+					node.parent.left = node.right;
+					node.right.parent = node.parent;
+				}
+			}
+		}
+		
+		// needs to implements, to finish this exercise
+		remove(value, root);	
+		
+		return node;
+	}
+
 }
